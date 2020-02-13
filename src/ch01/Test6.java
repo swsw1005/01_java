14076e654a4a77a3c444f3fa7a8af11bc3e5ae21
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-13 09:24:13 +0900

    Update Test6.java

--- src/ch01/Test6.java
+++ src/ch01/Test6.java
@@ -1,20 +1,34 @@
 package ch01;
 
+import java.util.Scanner;
+
 //"Test6.java"
 public class Test6 {
   public static void main(String args[]) {
-    int a = 10;
 
-    System.out.println(a);// 10진수
-    System.out.println(Integer.toBinaryString(a));// 2진수
-    System.out.println(Integer.toOctalString(a));// 8진수
-    System.out.println(Integer.toHexString(a) + "\n");// 16진수
+    Scanner scan = new Scanner(System.in);
+    System.out.println("숫자 입력해라");
+    int a = scan.nextInt();
 
-    //
-    System.out.println(Integer.MIN_VALUE);// -2147483648
-    System.out.println(Integer.MAX_VALUE);// 2147483647
+    try {
+      System.out.println();
+      System.out.println("10진수\t" + a);// 10진수
+      System.out.println("2진수\t" + Integer.toBinaryString(a));// 2진수
+      System.out.println("8진수\t" + Integer.toOctalString(a));// 8진수
+      System.out.println("16진수\t" + Integer.toHexString(a) + "\n");// 16진수
+      System.out.println();
+      //
+      System.out.println(Integer.MIN_VALUE);// -2147483648
+      System.out.println(Integer.MAX_VALUE);// 2147483647
+      System.out.println();
+      System.out.println(Long.MIN_VALUE);
+      System.out.println(Long.MAX_VALUE);
+    } catch (Exception r) {
+      // TODO: handle exception
+      System.out.println("숫자입력하라니까 임마");
 
-    System.out.println(Long.MIN_VALUE);
-    System.out.println(Long.MAX_VALUE);
+    }
+
+    scan.close();
   }// main end
 }// class end
