6aff0d047313eee03e678d5c8da4da97b15426f5
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-15 00:29:50 +0900

    Create sw3.java

---
+++ src/swsw/ch01/sw3.java
@@ -0,0 +1,20 @@
+package swsw.ch01;
+
+public class sw3 {
+
+    public static void main(String[] args) {
+        String[] mynum = new String[] { "2013-12-12", "2015-05-14", "2020-01-29", "2012-06-04" };
+
+        for (int i = 0; i < 10000; i++) {
+            System.out.print("mynum[" + i + "]\t");
+            try {
+                System.out.println(mynum[i]);
+            } catch (Exception e) {
+                System.out.println("invalid input");
+                System.out.println("bye bye");
+                break;
+            }
+        }
+
+    }
+}
