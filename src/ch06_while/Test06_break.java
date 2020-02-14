d868da745ca143bd42c661b0b0e414ffeab0a9fa
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-14 12:40:26 +0900

    123

--- src/ch06_while/Test06_break.java
+++ src/ch06_while/Test06_break.java
@@ -9,5 +9,20 @@ public class Test06_break {
             if (i == 10)
                 break;
         }
-    }
-}
+        System.out.println("----------");
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(j + " ");
+                // 원래 1~10 출력해야 하지만...
+                if (j == 5)
+                    break;
+                // 5에 다다르면 inner for 빠져나가버린다.
+            } // inner for end
+            System.out.println();
+            // 이것도 10줄 반복되어야 하지만...
+            if (i == 3)
+                break;// 3줄만 하고끝내버린다.
+        } // out for end
+    }// main end
+}// class end
