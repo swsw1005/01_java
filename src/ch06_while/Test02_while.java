64c4ad9767e41ded3093201bba9c411ef7c9bbd2
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-14 11:31:51 +0900

    1

---
+++ src/ch06_while/Test02_while.java
@@ -0,0 +1,15 @@
+package ch06_while;
+
+public class Test02_while {
+
+    public static void main(String[] args) {
+        // 1~20까지 짝수 출력
+        int i = 1;
+        while (i <= 20) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+    }
+}
