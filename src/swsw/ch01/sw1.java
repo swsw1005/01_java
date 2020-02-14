59b74a9e1b49b04d9f49e322a606aef468293699
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-15 00:10:35 +0900

    12

---
+++ src/swsw/ch01/sw1.java
@@ -0,0 +1,52 @@
+package swsw.ch01;
+
+public class sw1 {
+
+    public static void main(String[] args) {
+        // 006 문자열과 배열
+        String city1 = "Seoul";
+        String city2 = "busan";
+        System.out.println(city1 + " " + city2);
+        System.out.println();
+
+        city2 = city1;
+        System.out.println(city1 + " " + city2);
+
+        System.out.println(city1.length());
+
+        String city4 = String.format("%s-%s", city1, city2);
+
+        System.out.println(city4);
+
+        int[] nums2 = { 1, 2, 3 };
+        // new없이 생성된 배열...절대 변경불가
+
+        System.out.println(nums2[1]);
+        System.out.println();
+
+        int[] lotto = new int[6]; // {0,0,0,0,0,0}
+
+        System.out.println(lotto[0]);
+        System.out.println(lotto[1]);
+        System.out.println(lotto[2]);
+        System.out.println(lotto[3]);
+        System.out.println(lotto[4]);
+        System.out.println(lotto[5]);
+        // lotto[0] = -1; // -1 넣기,{-1,0,0,0,0,0}
+        lotto = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 };
+        System.out.println();
+
+        for (int i = 0; i < 1000000; i++) {
+
+            System.out.print("lotto[" + i + "]\t");
+
+            try {
+                System.out.println(lotto[i]);
+            } catch (Exception e) {
+                System.out.println("no int bye bye");
+                break;
+            } // try catch end
+        } // for end
+    }// main end
+}// class end
---
+++ src/swsw/ch01/sw2.java
@@ -0,0 +1,60 @@
+package swsw.ch01;
+
+import java.util.Scanner;
+
+public class sw2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("다른 방식으로 다시 만들어봐라");
+        System.out.println("다이아몬드");
+
+        System.out.println();
+        System.out.print("몇층짜리? >> ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        n = (int) n / 2;
+        System.out.println();
+        System.out.println(n + "층짜리");
+        System.out.println();
+
+        for (int i = 0; i <= 2 * n; i++) {
+
+            if (i <= n) {
+
+                for (int j = 0; j <= n - i - 1; j++) {
+                    System.out.print("☆");
+                }
+
+                for (int j = 0; j <= 2 * i; j++) {
+                    System.out.print("★");
+                }
+                for (int j = 0; j <= n - i - 1; j++) {
+                    System.out.print("☆");
+                }
+                System.out.println();
+
+            } else {
+
+                for (int j = 0; j <= i - n - 1; j++) {
+                    System.out.print("☆");
+                }
+
+                for (int j = 0; j <= 2 * (2 * n - i); j++) {
+                    System.out.print("★");
+                }
+                for (int j = 0; j <= i - n - 1; j++) {
+                    System.out.print("☆");
+                }
+
+                System.out.println();
+
+            }
+
+        }
+        System.out.println("-------------------------");
+        System.out.println();
+        scan.close();
+
+    }
+}
