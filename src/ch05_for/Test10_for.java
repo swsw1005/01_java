a1458368a64ca79a2a8a44d10c1d30f483896b48
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-14 11:06:27 +0900

    13

---
+++ src/ch05_for/Test10_for.java
@@ -0,0 +1,25 @@
+package ch05_for;
+
+import java.util.Scanner;
+
+public class Test10_for {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("몇단까지 보여줄까영 >>");
+        int n = scan.nextInt();
+        // 구구단
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 2; j <= n; j++) {
+
+                System.out.print(j + "*" + i + "=" + (j * i) + "\t");
+
+            } // inner for
+            System.out.println();
+        } // out for
+
+        scan.close();
+    }// main out
+}// class end
