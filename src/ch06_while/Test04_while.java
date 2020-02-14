fd622fa77d7f9f0597d380de16d095a53a31fd71
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-14 12:12:40 +0900

    1

--- src/ch06_while/Test04_while.java
+++ src/ch06_while/Test04_while.java
@@ -1,21 +1,22 @@
 package ch06_while;
 
-public class Test03_dowhile {
+public class Test04_while {
 
     public static void main(String[] args) {
-        // 초기치;
-        // do {
-        // 반복수행 내용
-        // } while (조건);
-        // do while은 뭐가 다른가?
-        // 무조건 한번은 수행한다.
         int i = 4;
+        int sum = 0;
         do {
             if (i % 7 == 0) {
                 System.out.print(i + " ");
+                sum += i;
             }
             i++;
         } while (i <= 90);
 
+        System.out.println();
+
+        System.out.println("sum : " + sum);
+
     }
+
 }
