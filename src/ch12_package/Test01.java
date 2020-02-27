b0a4aae82bb740ec1c1d9502a89b2b3e670c417c
swsw <swsw1005@gmail.com>
2020-02-27 12:00:17 +0900

    555

---
+++ src/ch12_package/Test01.java
@@ -0,0 +1,18 @@
+package ch12_package;
+
+// package hong.kil.dong;
+
+import hong.kil.dong.*;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        CalcTest01 t1 = new CalcTest01();
+        CalcTest02 t2 = new CalcTest02();
+        
+        
+        t1.hab(13, 15);
+        t2.hab2(20, 40);
+    }
+
+}
